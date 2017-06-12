a89ff7923991188a41a2e2b1a03162d5db244fc6
Ankit <tcs.unitedairlines@gmail.com>
2017-06-12 12:02:43 +0530

    Initial Commit

---
+++ QoLPOC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QoLPOC
+//
+//  Created by United TCS Mini on 12/06/17.
+//  Copyright © 2017 United TCS Mini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
